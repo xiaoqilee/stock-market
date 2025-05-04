fa836c3597e2f6a3684b99d85a544b90511d029a
LilaNguyen <lknguyen2019@gmail.com>
2025-05-04 03:03:19 -0700

    Implemented summary stats functionality. Updated UI.

--- homepage.py
+++ homepage.py
@@ -5,6 +5,8 @@ from tkinter import *
 import customtkinter as ctk # for the modern twist!
 import yfinance as yf
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # set mode and theme
 ctk.set_appearance_mode("Dark")
@@ -31,21 +33,21 @@ class Homepage:
         mainframe.columnconfigure(0, weight=1)
 
         # define variables for home screen labels
-        self.stock_ticker = StringVar()
+        self.ticker = StringVar()
         self.graph = StringVar()
-        self.summary_stats = StringVar()
+        self.stats = StringVar()
 
         # add a label in the GUI for the stock ticker
-        label = ctk.CTkLabel(mainframe, text='Enter Stock Ticker:', text_color="#565b5e")
-        label.grid(column=1, row=1, padx=20, pady=5)
+        label = ctk.CTkLabel(mainframe, text='Enter Stock Ticker:', text_color="#323339")
+        label.grid(column=1, row=1, padx=20, pady=5, sticky="w")
 
         # add an Entry in the GUI for the stock ticker
-        self.entry = ctk.CTkEntry(mainframe, textvariable=self.stock_ticker, fg_color="#fdfaff", border_color="#ecaec5", text_color="#e26c99")
+        self.entry = ctk.CTkEntry(mainframe, textvariable=self.ticker, fg_color="#fdfaff", border_color="#ecaec5", text_color="#e26c99")
         self.entry.grid(column=1, row=2, padx=20, pady=5)
 
         # add a label in the GUI for the time range
-        label = ctk.CTkLabel(mainframe, text='Select Time Range:', text_color="#565b5e")
-        label.grid(column=1, row=3, padx=20, pady=5)
+        label = ctk.CTkLabel(mainframe, text='Select Time Range:', text_color="#323339")
+        label.grid(column=1, row=3, padx=20, pady=5, sticky="w")
 
         # add a ComboBox to the first frame and add it to the GUI
         self.combo = ctk.CTkComboBox(mainframe, values=['1 Day', '5 Days', '1 Month', '1 Year'], fg_color="#fdfaff", border_color="#ecaec5", text_color="#e26c99")
@@ -58,12 +60,9 @@ class Homepage:
         self.generate_button = ctk.CTkButton(mainframe, text="Generate", command=self.generate, fg_color="#ecaec5", text_color="#fdfaff")
         self.generate_button.grid(row=5, column=1, padx=20, pady=5)
 
-        # add spacing above and below Button/Widgets in 8th row
-        mainframe.rowconfigure(8, weight=1)
-
         # add label for export
-        format_label = ctk.CTkLabel(mainframe, text="Select Export Format:", text_color="#565b5e")
-        format_label.grid(row=1, column=2, padx=20, pady=5)
+        format_label = ctk.CTkLabel(mainframe, text="Select Export Format:", text_color="#323339")
+        format_label.grid(row=1, column=2, padx=20, pady=5, sticky="w")
 
         # add combo box with options of CSV or JSON
         self.format_combo = ctk.CTkComboBox(mainframe, values=["CSV", "JSON"], fg_color="#fdfaff", border_color="#ecaec5", text_color="#e26c99")  # Adjust width as needed
@@ -76,12 +75,12 @@ class Homepage:
 
         # add a button to close the app
         self.close_button = ctk.CTkButton(mainframe, text="Close App", command=root.destroy, fg_color="#565b5e", text_color="#fdfaff")
-        self.close_button.grid(row=5, column=2, pady=10)
+        self.close_button.grid(row=5, column=2, padx=20, pady=10)
 
 
     def generate(self):
         # change ticker to uppercase and get the selected range form the combo box 
-        self.ticker = self.entry.get().upper() 
+        self.ticker = self.entry.get().upper()
         time_range = self.combo.get()
 
         # assign a period and interval based on the time range selected
@@ -104,19 +103,58 @@ class Homepage:
         # get the data from yahoo finance
         try:
             self.data = yf.download(self.ticker, period=period, interval=frequency) # download data from yf as a pd df
-            self.stats(self.ticker, self.data)
+            self.graph_and_stats(self.ticker, self.data)
         except Exception as e:
                 pass
-        
-    # open new window for stats
-    def stats(self, ticker, data):
-        popup_window = ctk.CTkToplevel()
-        popup_window.title(f"Statistics for {self.ticker}")
-        
-        ctk.CTkLabel(popup_window, text="temp").pack(padx=20, pady=20)
 
-        label = ctk.CTkLabel(popup_window, text="Temp placeholder")
-        label.grid(column=0, row=2, sticky="news")
+    # open new window for graph and summary statistics
+    def graph_and_stats(self, ticker, data):
+        popup_window = ctk.CTkToplevel()
+        popup_window.title(f"Graph & Summary Stats for {self.ticker}")
+        popup_window.geometry("475x500")
+        popup_window.configure(fg_color="#e6d8f3") # change bg color
+
+        # set font styles
+        title_style = ("TkDefaultFont", 14, "bold")
+        headers_style = ("TkDefaultFont", 12, "bold")
+        values_style = ("TkDefaultFont", 12)
+
+        # get summary stats (mean, max, min closing price)
+        try:
+            mean_close_price = "{:.2f}".format(data["Close", ticker].mean())
+            max_close_price = "{:.2f}".format(data["Close", ticker].max())
+            min_close_price = "{:.2f}".format(data["Close", ticker].min())
+        except Exception as e:
+            print("Error computing summary stats: ", e)
+
+        # create labels for summary stats
+        time_range = self.combo.get()
+        label = ctk.CTkLabel(popup_window, text="Closing Summary ("+time_range+")", text_color="#323339", font=title_style)
+        label.grid(row=0, column=0, padx=20, pady=10)
+
+        mean_label = ctk.CTkLabel(popup_window, text="Mean Price:", text_color="#323339", font=headers_style)
+        mean_label.grid(row=1, column=0, padx=20, pady=5, sticky="w")
+
+        max_label = ctk.CTkLabel(popup_window, text="Max Price:", text_color="#323339", font=headers_style)
+        max_label.grid(row=2, column=0, padx=20, pady=5, sticky="w")
+
+        min_label = ctk.CTkLabel(popup_window, text="Min Price:", text_color="#323339", font=headers_style)
+        min_label.grid(row=3, column=0, padx=20, pady=5, sticky="w")
+
+        # assign values to the respective labels
+        mean_val = ctk.CTkLabel(popup_window, text='$'+mean_close_price, text_color="#323339", font=values_style)
+        mean_val.grid(row=1, column=1, padx=20, pady=5, sticky="w")
+
+        max_val = ctk.CTkLabel(popup_window, text='$'+max_close_price, text_color="#323339", font=values_style)
+        max_val.grid(row=2, column=1, padx=20, pady=5, sticky="w")
+
+        min_val = ctk.CTkLabel(popup_window, text='$'+min_close_price, text_color="#323339", font=values_style)
+        min_val.grid(row=3, column=1, padx=20, pady=5, sticky="w")
+
+        # add a button to close the window
+        self.close_button = ctk.CTkButton(popup_window, text="Close Window", command=popup_window.destroy, fg_color="#debbe7", text_color="#fdfaff")
+        self.close_button.grid(column=0, row=6, padx=20, pady=10, sticky="w")
+
 
     # download as csv or json into current directory based on chosen data format
     def download(self):
@@ -130,6 +168,7 @@ class Homepage:
                 self.data.to_json(filename, orient='records')
         except Exception as e:
             pass
+
 
 # create a root Tk object
 root = ctk.CTk()
